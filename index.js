a5a4d3350346cf1f0917386823e23a259cfc41d9
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-09-21 09:01:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(target = "roller-skate") {
+    return `This Saturday, I want to ${target}!`;
+
+}
+
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective(highlight = "||"){
+return function innerfunction(adjective = "special"){
+    return `You are ${highlight}${adjective}${highlight}!`;
+};
+return `You are ${highlight}a dedicated programmer${highlight}!`;
+}
